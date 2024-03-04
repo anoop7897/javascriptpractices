6f1fc5554be9041b15fd3f74af9ac3ed6a6ea005
anoop7897 <82763342+anoop7897@users.noreply.github.com>
2024-03-04 06:17:55 +0000

    javascript variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+const accountId = 123456;
+let accountEmail = "anoopm45@gmail.com";
+var accountCity = "Raebareli";
+let name,admin;
+name = "John";
+admin = name;
+const myPlanetName = "Earth";
+let currentVisitor = "Alok Maurya"
+console.log(myPlanetName);
+console.log(currentVisitor);
+console.log("Username:",name);
+console.log("Admin name is:",admin);
+console.table([accountId,accountEmail,accountCity,name,admin]);
